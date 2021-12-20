7e04fb5a5cafd1466e72565210dd2b00c360b769
Elina Widdowson <elina.widdowson@tuni.fi>
2021-12-20 13:21:01 +0200

    Add handleFormSubmit(); set up name state

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
-export default function Form() {
+export default function Form(props) {
+    // For name state, set initial state to empty string, create setName function to update name state
+    const [name, setName] = useState('');
+
+    // Handle submitting form data from user
+    function handleFormSubmit(e) {
+        e.preventDefault();
+        props.addNewTask("Going to add new task!"); // debugging
+    }
+
     return (
-        <form>
+        <form onSubmit={handleFormSubmit}>
             <h2>
                 <label htmlFor="input-new-todo" className="label">
                     Add a task:
@@ -14,6 +23,7 @@ export default function Form() {
                 className="input"
                 name="text"
                 autoComplete="off"
+                value={name}
             />
             <button type="submit" className="button-submit">
                 Add task
