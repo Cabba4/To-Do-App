d3d7a6045b920b1eda366f11ea15e5d6e916b6d5
Elina Widdowson <elina.widdowson@tuni.fi>
2021-12-25 12:50:44 +0200

    Fetch random todos from api endpoint

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,18 +3,47 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {logDOM} from "@testing-library/react";
 
-const TODO_TASKS = [
-    {id: "todo-0", name:"Go to the grocery store", completed: false},
-    {id: "todo-1", name:"Read a book", completed: false},
-    {id: "todo-2", name:"Bake a birthday cake", completed: false},
+//const apiEndpoint = "http://21wsp8pw.course.tamk.cloud/api/v1/tasks/random";
+const apiEndpoint = "http://localhost:5050/api/v1/tasks/random";
+
+let todoTasks = [
+    {id: "todo-0", name: "Go to the grocery store", completed: false},
+    {id: "todo-1", name: "Read a book", completed: false},
+    {id: "todo-2", name: "Bake a birthday cake", completed: false},
 ];
 
+async function fetchEndpointPromise(endpoint) {
+    const result = await fetch(endpoint);
+    return await result.json();
+}
+
+function fetchTodoPromise(endpoint) {
+    return Promise.resolve(fetchEndpointPromise(endpoint).then(data => {
+        let todoObject = data.message[0];
+        let firstProperty = todoObject[Object.keys(todoObject)[0]];
+        return firstProperty;
+    }));
+}
+
+function updateTodoTasks() {
+    todoTasks.forEach(todo => {
+        fetchTodoPromise(apiEndpoint).then(todoName =>
+            todo.name = todoName
+        );
+    })
+
+    console.log("Updated todoTasks to: ", todoTasks)
+}
+
+updateTodoTasks();
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App tasks={TODO_TASKS}/>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App tasks={todoTasks}/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
