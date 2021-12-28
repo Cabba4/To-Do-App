8250c31d926dddcf3c070e32e136173615b4bef0
Elina Widdowson <elina.widdowson@tuni.fi>
2021-12-28 13:39:44 +0200

    Modify and add unit tests for frontend. Closes #12

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,14 +1,30 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders "To-do List"', () => {
-  const TODO_TASKS = [
-    {id: "todo-0", name:"Task 0", completed: true},
-    {id: "todo-1", name:"Task 1", completed: false},
-    {id: "todo-2", name:"Task 2", completed: false},
-  ];
+test('renders first heading as "To-do List"', () => {
+  render(<App/>);
+  const headingText = screen.getByRole('heading', {level: 1});
+  expect(headingText).toHaveTextContent('To-do List');
+});
 
-  render(<App tasks={TODO_TASKS}/>);
-  const headingText = screen.getByText("To-do List");
-  expect(headingText).toBeInTheDocument();
+test('renders button to add task', () => {
+  const result = render(<App/>);
+  const buttonElement = result.container.querySelector(".button-submit");
+  expect(buttonElement).toBeInTheDocument();
+});
+
+test('renders two headings', () => {
+ render(<App />);
+    expect(screen.getAllByRole('heading')).toHaveLength(2);
+});
+
+test('renders second heading as "Current Tasks"', () => {
+    render(<App />);
+    expect(screen.getByRole('heading', {level: 2})).toHaveTextContent('Current Tasks');
+});
+
+test('renders input textbox for submission form', () => {
+    render(<App/>);
+    const listItem = screen.getByRole('textbox');
+    expect(listItem).toBeInTheDocument();
 });
